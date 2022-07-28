52449dd2fc6e8c587fd3bce3c178513131e2f28e
Dimitri Sifoua <dimitri.sifoua@gmail.com>
2022-07-28 18:12:39 -0400

    Add the stacked bidirectionnal lstm layer

--- question_answering/drqa/layers/stacked_bidirectional_lstm_layer.py
+++ question_answering/drqa/layers/stacked_bidirectional_lstm_layer.py
@@ -7,7 +7,16 @@ from typing import Tuple
 class StackedBiLSTMsLayer(nn.Module):
 
     def __init__(self, embedding_size: int, hidden_size: int, n_layers: int, dropout: float):
-        super(StackedBiLSTMsLayer).__init__()
+        """
+        :param embedding_size: Size of word embedding.
+        :param hidden_size: Hidden size of lstm layers.
+        :param n_layers: Number of layers.
+        :param dropout: Dropout value in [0, 1).
+        """
+        super(StackedBiLSTMsLayer, self).__init__()
+        if dropout < 0 or dropout >= 1:
+            raise ValueError("Dropout must be in [0, 1)")
+
         self.embedding_size = embedding_size
         self.hidden_size = hidden_size
         self.n_layers = n_layers
@@ -22,15 +31,31 @@ class StackedBiLSTMsLayer(nn.Module):
 
     def __forward_lstm(self, layer: nn.Module, inputs: torch.Tensor, lengths: torch.Tensor) \
             -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :param layer: LSTM layer.
+        :param inputs: Sequence inputs. FloatTensor[batch_size, max_sequence_length, embedding_size|hidden_size * 2]
+        :param lengths: Sequence lengths. LongTensor[batch_size,]
+        :return:
+            padded_outputs FloatTensor[batch_size, max_sequence_length, hidden_size * 2]
+            outputs_lengths LongTensor[batch_size,]
+        """
         inputs = self.dropout(inputs)
         packed_inputs = pack_padded_sequence(input=inputs, lengths=lengths, batch_first=True, enforce_sorted=False)
         packed_outputs, _ = layer(packed_inputs)
         padded_outputs, outputs_lengths = pad_packed_sequence(sequence=packed_outputs, batch_first=True)
         return padded_outputs, outputs_lengths
 
-    def forward(self, input_embedded: torch.Tensor, sequence_lengths: torch.Tensor) -> torch.Tensor:
-        outputs, lens = [input_embedded], sequence_lengths
+    def forward(self, embedded_inputs: torch.Tensor, sequence_lengths: torch.Tensor) -> torch.Tensor:
+        """
+        :param embedded_inputs: Sequence inputs. FloatTensor[batch_size, max_sequence_length, embedding_size]
+        :param sequence_lengths: Sequence lengths. LongTensor[batch_size,]
+        :return: FloatTensor[batch_size, sequence_lengths, n_layers * hidden_size * 2]
+        """
+        if embedded_inputs.size()[0] != sequence_lengths.size()[0]:
+            raise ValueError("Batch sizes (1st dimension) of inputs must match")
+
+        outputs, lens = [embedded_inputs], sequence_lengths
         for lstm_layer in self.lstm_layers:
-            out, lens = self.apply_lstm(layer=lstm_layer, inputs=outputs[-1], lengths=lens)
+            out, lens = self.__forward_lstm(layer=lstm_layer, inputs=outputs[-1], lengths=lens)
             outputs.append(out)
         return self.dropout(torch.cat(outputs[1:], dim=-1))
--- tests/drqa/test_stacked_bidirectional_lstm_layer.py
+++ tests/drqa/test_stacked_bidirectional_lstm_layer.py
@@ -1,16 +1,36 @@
-import unittest
-import numpy as np
 import torch
+import unittest
 from question_answering.drqa.layers import StackedBiLSTMsLayer
 
 
 class TestStackedBiLSTMsLayer(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.embedding_size = 300
+        self.hidden_size = 64
+        self.n_layers = 8
+        self.dropout = 0.5
+
+        self.stack_bilstms_layer = StackedBiLSTMsLayer(embedding_size=self.embedding_size, hidden_size=self.hidden_size,
+                                                       n_layers=self.n_layers, dropout=self.dropout)
 
     def test_init_layer(self):
-        pass
+        with self.assertRaises(ValueError):
+            _ = StackedBiLSTMsLayer(embedding_size=self.embedding_size, hidden_size=self.hidden_size,
+                                    n_layers=self.n_layers, dropout=1)
 
     def test_forward(self):
-        pass
+        max_sequence_length, batch_size = 50, 32
+        with self.assertRaises(ValueError):
+            _ = self.stack_bilstms_layer(
+                embedded_inputs=torch.randn(size=(batch_size, max_sequence_length, self.embedding_size)),
+                sequence_lengths=torch.randint(low=1, high=max_sequence_length, size=(batch_size + 1,))
+            )
+
+        bilstm_outputs = self.stack_bilstms_layer(
+            embedded_inputs=torch.randn(size=(batch_size, max_sequence_length, self.embedding_size)),
+            sequence_lengths=torch.randint(low=1, high=max_sequence_length + 1, size=(batch_size,))
+        )
+        self.assertEqual(
+            bilstm_outputs.size(), torch.Size([batch_size, max_sequence_length, self.n_layers * self.hidden_size * 2])
+        )
